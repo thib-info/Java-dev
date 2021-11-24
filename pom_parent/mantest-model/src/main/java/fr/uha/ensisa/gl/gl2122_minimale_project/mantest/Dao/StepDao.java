f8ad50abfb955c2fed4d0baa009ba89fd650951e
thib_info <thibault.b45@gmail.com>
2021-11-24 10:17:49 +0100

    The base of the project for the first issues

--- pom_parent/mantest-model/src/main/java/fr/uha/ensisa/gl/gl2122_minimale_project/mantest/Dao/StepDao.java
+++
@@ -1,5 +0,0 @@
-package fr.uha.ensisa.gl.gl2122_minimale_project.mantest.Dao;
-
-public interface StepDao extends ModelDao {
-
-}
--- pom_parent/mantest-model/src/main/java/fr/uha/ensisa/gl/gl2122_minimale_project/mantest/Dao/SystemDao.java
+++ pom_parent/mantest-model/src/main/java/fr/uha/ensisa/gl/gl2122_minimale_project/mantest/Dao/SystemDao.java
@@ -1,11 +1,5 @@
 package fr.uha.ensisa.gl.gl2122_minimale_project.mantest.Dao;
 
 public interface SystemDao extends ModelDao {
-	public void addTest(TestDao test);
-	public void removeTest(TestDao test);
-	public void modifyTest(long id, TestDao test);
-	public TestDao getTest(long id);
-	public StoreDao<TestDao> allTest();
-	public long countTest();
 
 }
