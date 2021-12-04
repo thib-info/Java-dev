4d6f41fd80eebfddf064972fcac936d4f798a35a
thib_info <thibault.b45@gmail.com>
2021-12-04 23:38:57 +0100

    #27 Start deletion implementation

---
+++ pom_parent/mantest-app/src/main/java/fr/uha/ensisa/gl/minimale/mantest_app/controller/RemoveTestController.java
@@ -0,0 +1,25 @@
+package fr.uha.ensisa.gl.minimale.mantest_app.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import fr.uha.ensisa.gl.gl2122_minimale_project.mantest.Dao.DaoFactory;
+import fr.uha.ensisa.gl.gl2122_minimale_project.mantest.Dao.SystemDao;
+import fr.uha.ensisa.gl.gl2122_minimale_project.mantest.Dao.TestDao;
+
+
+@Controller
+public class RemoveTestController {
+	@Autowired 
+	public DaoFactory factory;
+	
+	@ResponseBody
+	@RequestMapping(value="/removeTest")
+	public void removeTest(@RequestParam(required=true) int idSystem, @RequestParam(required=true) int idTest) {
+		SystemDao sus = this.factory.getSystemsStore().getStore().get(idSystem);
+		TestDao currentTest = (TestDao) sus.getStoreTest().getStore().get(idTest);
+		sus.getStoreTest().remove(currentTest);
+	}
+}
