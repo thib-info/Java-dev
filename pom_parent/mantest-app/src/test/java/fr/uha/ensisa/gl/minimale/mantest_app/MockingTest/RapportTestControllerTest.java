2f9981c30186045a49fd949146b2a6308dd09891
thib_info <thibault.b45@gmail.com>
2021-12-12 19:18:42 +0100

    100% Coverage Mockito

--- pom_parent/mantest-app/src/test/java/fr/uha/ensisa/gl/minimale/mantest_app/MockingTest/RapportTestControllerTest.java
+++ pom_parent/mantest-app/src/test/java/fr/uha/ensisa/gl/minimale/mantest_app/MockingTest/RapportTestControllerTest.java
@@ -1,66 +1,69 @@
 package fr.uha.ensisa.gl.minimale.mantest_app.MockingTest;
 
-import static org.junit.jupiter.api.Assertions.*;
-import org.springframework.web.servlet.ModelAndView;
-
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.verify;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.when;
 
-import java.io.IOException;
+import java.util.Collection;
 import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.web.servlet.ModelAndView;
 
 import fr.uha.ensisa.gl.gl2122_minimale_project.mantest.Dao.DaoFactory;
 import fr.uha.ensisa.gl.gl2122_minimale_project.mantest.Dao.StoreDao;
 import fr.uha.ensisa.gl.gl2122_minimale_project.mantest.Dao.SystemDao;
 import fr.uha.ensisa.gl.gl2122_minimale_project.mantest.Dao.TestDao;
-import fr.uha.ensisa.gl.minimale.mantest_app.controller.AddTestController;
-import fr.uha.ensisa.gl.minimale.mantest_app.controller.SystemUnitController;
+import fr.uha.ensisa.gl.gl2122_minimale_project.mantest.Dao.TestResultDao;
+import fr.uha.ensisa.gl.minimale.mantest_app.controller.RapportTestController;
 
-class AddTestControllerTest {
-
+class RapportTestControllerTest {
+	
 	@Mock DaoFactory daoFactory;
 	@Mock SystemDao system;
 	@Mock StoreDao<SystemDao> storeSystem;
 	@Mock List<SystemDao> listSystem;
 	@Mock StoreDao<TestDao> storeTest;
 	@Mock List<TestDao> listTest;
-	public AddTestController sut;
-	String titleTest;
+	@Mock TestDao test;
+	@Mock TestResultDao resTest;
+	public RapportTestController sut;
 
 	@BeforeEach
 	public void prepareDao() {
 		MockitoAnnotations.openMocks(this);
-		this.titleTest = "Test1";
 	}
-
+	
 	@BeforeEach
-	public void initSystems() {
+	public void initSystems() {		
 		when(daoFactory.getSystemsStore()).thenReturn(this.storeSystem);
 		when(daoFactory.getSystemsStore().getStore()).thenReturn(this.listSystem);
 		when(daoFactory.getSystemsStore().getStore().get(0)).thenReturn(this.system);
 	}
-
+	
 	@BeforeEach
 	public void initTest() {
 		when(this.system.getStoreTest()).thenReturn(storeTest);
+		when(this.storeTest.getStore()).thenReturn(listTest);
+		when(this.listTest.get(0)).thenReturn(test);
+		when(this.test.getResult()).thenReturn(resTest);
 	}
-
+	
 	@BeforeEach
 	public void initController() {
-		sut = new AddTestController();
+		sut = new RapportTestController();
 		sut.factory = this.daoFactory;
 	}
-	
+
 	@Test
-	public void createTest() throws IOException {
-		sut.addTest(0, this.titleTest, null);
-		verify(this.storeTest).save(any(fr.uha.ensisa.gl.gl2122_minimale_project.mantest.Dao.TestDao.class));
+	public void getRapportTest() {
+		ModelAndView ret = sut.rapportTestPresentation(0, 0);
+		TestResultDao resultTest = (TestResultDao) ret.getModelMap().get("result");
+		assertNotNull(resultTest);
+		assertTrue(resultTest.getClass().getName() == this.resTest.getClass().getName());
 	}
-	
+
 }
